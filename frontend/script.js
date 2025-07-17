648909af25714ce64a164568dbfc9a37b1ce4859
tanujsaini123 <148194541+tanujsaini123@users.noreply.github.com>
2025-07-17 16:35:11 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,8 +0,0 @@
-async function fetchQuote() {
-  const res = await fetch('/quote');
-  const data = await res.json();
-  document.getElementById('quote').innerText = `"${data.quote}"`;
-  document.getElementById('author').innerText = `— ${data.author}`;
-}
-
-fetchQuote();
